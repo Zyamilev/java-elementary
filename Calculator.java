a5d2589a5fa36c5bd11c5a8656f8f605db531de4
Evgen <zyamilev@mail.ru>
2019-12-04 21:25:22 +0200

    First Commit

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,8 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main(String[] args)
+    {
+        System.out.println("123");
     }
-
-
 }
