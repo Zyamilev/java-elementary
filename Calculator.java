81ef8d33e1866e34dd1d50af943c35055a785624
Evgen <zyamilev@mail.ru>
2019-12-10 20:32:19 +0200

    04-12

--- Calculator.java
+++ Calculator.java
@@ -4,15 +4,19 @@ public class Calculator {
     public static void main(String[] args) {
         double num_1;
         double num_2;
-        System.out.println("Введите первое число");
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
+        System.out.println("Введите первое число");
         num_1 = scanner.nextDouble();
-        System.out.println("Веедите второе число");
+        System.out.println("Введите второе число");
         num_2 = scanner.nextDouble();
         double umn = num_1 * num_2;
         double del = num_1 / num_2;
+        double sum = num_1 + num_2;
+        double minus = num_1 - num_2;
         System.out.println("деление " + num_1 + " на " + num_2 + " равно " + del);
         System.out.println("умножение " + num_1 + " на " + num_2 + " равно " + umn);
+        System.out.println("сумма " + num_1 + " и " + num_2 + " равна " + sum);
+        System.out.println("вычитание " + num_1 + " от " + num_2 + " равно " + minus);
     }
 }
